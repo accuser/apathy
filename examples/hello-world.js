19b97e107d520bd1aff3028e58d7e922f521a3ae
Matthew Gibbons <mhgibbons@me.com>
2023-11-23 11:59:55 +0000

    Updated example to reflect API changes

--- examples/hello-world.js
+++ examples/hello-world.js
@@ -1,14 +1,14 @@
 import apathy from "apathy";
-import { cert, key } from "./env.js";
+import { cert, host, key, port } from "./env.js";
 
 apathy({ allowHTTP1: true, cert, key })
-	.use(({ method, httpVersion, url }) => {
+	.use("/", ({ method, httpVersion, url }) => {
 		console.log(`${method} ${url} HTTP/${httpVersion}`);
 	})
-	.get((request, response) => {
+	.get("/", (request, response) => {
 		response.end(`Hello, World!`);
 	})
-	.listen(({ address }) => {
+	.listen(port, host, ({ address }) => {
 		if (address === null) {
 			// do nothing
 		} else if (typeof address === "string") {
